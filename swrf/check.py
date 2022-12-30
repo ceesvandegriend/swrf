47d5814e91bb0987edda90530b85b2494832b2e8
Cees van de Griend <cees@griend.eu>
2022-12-30 11:52:50 +0100

    Check URL

--- swrf/check.py
+++ swrf/check.py
@@ -1,88 +1,110 @@
 """
 Check an URL for connection.
 
-Author:    Cees van de Griend <cees@griend.eu>
-Date:      28 dec 2022
 Copyright: (c) 2022 C.A. van de Griend
 """
+
+from dataclasses import dataclass
+from datetime import datetime
 import logging
+import schedule
 import time
 import urllib3
-from urllib3.exceptions import InsecureRequestWarning, ConnectTimeoutError
+import urllib3.exceptions
 
 import requests
-from requests.exceptions import ConnectTimeout
+from requests.exceptions import ConnectionError
 
+from .common import Check
+from .database import get_database_filename
+from .database import database_insert_check
+
+__author__ = "Cees van de Griend <cees@griend.eu>"
+__status__ = "development"
+__version__ = "0.1"
+__date__ = "29 december 2022"
 
 logger = logging.getLogger(__name__)
 
 
-def check_url(url: str):
-    """
-    Check the URL a number of times.
+def check(name: str, url: str) -> Check:
+    logger.debug("check() - start")
 
-    Returns:
-      status_code[] - int
-      duration[] - float
-    """
-    logger.debug("check_url() - start")
+    check = Check()
+    check.time = datetime.now()
+    check.name = name
+    check.description = url
 
-    status = []
-    duration = []
-
-    for n in range(5):
+    try:
         t0 = time.perf_counter()
-        try:
-            response = requests.get(url, verify=False, timeout=0.5)
-            status_code = response.status_code
-        except InsecureRequestWarning as e:
-            pass
-        except ConnectTimeout as e:
-            status_code = -1
+        response = requests.get(url, verify=False, timeout=2.0)
         t1 = time.perf_counter()
+        status_code = response.status_code
+        duration = round((t1 - t0) * 1000)
+    except ConnectionError:
+        status_code = 0
+        duration = 0
 
-        status.append(status_code)
-        duration.append(t1 - t0)
+    check.duration = duration
 
-    logger.debug("check_url() - finish")
-
-    return status, duration
-
-
-def checks(name: str, url: str) -> None:
-    logger.debug("check() - start")
-
-    statuses, durations = check_url(url)
-
-    success = "Success"
-    avg = sum(durations) / len(durations)
-
-    for status in statuses:
-        if status != 200:
-            success = "Failure"
-
-    logger.info(f"{name} {success} {avg:0.4f}")
-    logger.debug("check() - finish")
-
-
-def check() -> None:
-    logger.debug("check() - start")
-
-    checks("UniFi", "https://192.168.1.1/")
-    checks("Genexis", "https://192.168.179.1/")
-    checks("Google", "https://www.google.nl/")
+    if status_code <= 0:
+        check.status = Check.RED
+    elif status_code >= 190 and status_code <= 210:
+        check.status = Check.GREEN
+    else:
+        check.status = Check.YELLOW
 
     logger.debug("check() - finish")
+
+    return check
+
+
+def publish(check: Check) -> None:
+    logger.debug("publish() - start")
+
+    if check.status == Check.GREEN:
+        logger.info(f"{check.name}: Green {check.duration} ms")
+    elif check.status == Check.YELLOW:
+        logger.warning(f"{check.name}: Yellow {check.duration} ms")
+    elif check.status == Check.RED:
+        logger.error(f"{check.name}: Red {check.duration} ms")
+    else:
+        logger.warning(f"{check.name}: White {check.duration} ms")
+        logger.warning(check)
+
+    filename = get_database_filename()
+    database_insert_check(filename, check)
+
+    logger.debug("publish() - finish")
+
+
+def main() -> None:
+    logger.debug("main() - start")
+
+    publish(check("GW", "https://192.168.128.1/"))
+    publish(check("UniFi", "https://192.168.1.1/"))
+    publish(check("Genexis", "https://192.168.179.1/"))
+    publish(check("Google", "https://www.google.com/"))
+
+    logger.debug("main() - finish")
 
 
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s %(levelname)-8s %(name)-12s %(message)s",
+        format="%(asctime)s %(levelname)s %(name)s - %(message)s",
     )
-    urllib3.disable_warnings(InsecureRequestWarning)
+
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
     try:
-        check()
+        main()
+        schedule.every(5).minutes.do(main)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(30)
+    except KeyboardInterrupt:
+        pass
     except Exception as e:
         logger.exception(e)
---
+++ swrf/database.py
@@ -0,0 +1,93 @@
+import logging
+import os
+from pathlib import Path
+import sqlite3
+
+from .common import Check
+
+__author__ = "Cees van de Griend <cees@griend.eu>"
+__status__ = "development"
+__version__ = "0.1"
+__date__ = "29 december 2022"
+
+CREATE_TABLE_SQL = """
+CREATE TABLE IF NOT EXISTS checks (
+    id          INTEGER   NOT NULL PRIMARY KEY AUTOINCREMENT,
+    time        TIMESTAMP NOT NULL,
+    name        TEXT      NOT NULL,
+    description TEXT      NOT NULL DEFAULT '',
+    status      INTEGER   NOT NULL DEFAULT 0,
+    duration    INTEGER   NOT NULL DEFAULT 0
+)
+"""
+
+INSERT_CHECK_SQL = """
+INSERT INTO checks (time, name, description, status, duration) VALUES(?, ?, ?, ?, ?)
+"""
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_database_filename():
+    dir = os.path.dirname(__file__)
+    prj_dir = os.path.dirname(dir)
+    var_dir = os.path.join(prj_dir, "var")
+    db_dir = os.path.join(var_dir, "db")
+    db_filename = os.path.join(db_dir, "swrf.db")
+
+    if not os.path.isdir(db_dir):
+        os.makedirs(db_dir)
+
+    return db_filename
+
+
+def database_create(filename: str):
+    logger.debug(f"database_create({filename}) - start")
+
+    print(CREATE_TABLE_SQL)
+
+    with sqlite3.connect(filename) as conn:
+        with conn as cursor:
+            cursor.execute(CREATE_TABLE_SQL)
+
+    logger.debug(f"database_create({filename}) - finish")
+
+
+def database_insert_check(filename: str, check: Check):
+    logger.debug(f"database_insert_check({filename}) - start")
+
+    data = (
+        check.time,
+        check.name,
+        check.description,
+        check.status,
+        check.duration,
+    )
+
+    with sqlite3.connect(filename) as conn:
+        with conn as cursor:
+            cursor.execute(INSERT_CHECK_SQL, data)
+
+    logger.debug(f"database_insert_check({filename}) - finish")
+
+
+def main() -> None:
+    logger.debug("main() - start")
+
+    filename = get_database_filename()
+    database_create(filename)
+
+    logger.debug("main() - finish")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(name)s - %(message)s",
+    )
+
+    try:
+        main()
+    except Exception as e:
+        logger.exception(e)
